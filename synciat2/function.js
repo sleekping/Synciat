ff3cb7be4a9fef14ce7e8507b38852152ac25e4a
https://github.com/kellumschain/contractchain.git <https://github.com/kellumschain/contractchain.git>
2023-08-22 23:30:24 -0400

    Nav notification bar

--- synciat2/function.js
+++ synciat2/function.js
@@ -1,1 +1,19 @@
-//this is for special dropdown effect on nav
+// Get references to the form and submit button
+
+function submitForm() {
+	const contentEmail = document.getElementById("email-form");
+   // const submitButton = document.getElementById("my-btn")
+
+// Add an event listener to the form submission
+contentEmail.addEventListener("submit", function (event) {
+    event.preventDefault();
+    
+
+    // Change the content of the message element and increase display size
+
+    contentEmail.innerHTML = "Thank you. Your code will automatically apply at checkout.";
+    contentEmail.style.fontSize = "15px";
+    contentEmail.style.color = "white";
+  });
+
+};
