74f07999384c05064265c08ea1a950bdf749aa4d
https://github.com/kellumschain/contractchain.git <https://github.com/kellumschain/contractchain.git>
2023-09-18 10:47:24 -0400

    please lets review nav bar on sign up page

--- synciat2/function.js
+++ synciat2/function.js
@@ -1,3 +1,9 @@
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById('signIn');
+const container = document.querySelector('.container');
+const form = document.querySelector(".form-container");
+const overlay = document.querySelector(".overlay-container");
+
 // Get references to the form and submit button on notification nav bar
 
 function submitForm() {
@@ -19,29 +25,59 @@ contentEmail.addEventListener("submit", function (event) {
 };
 
 
+
+function signMeButton() {
+  // Add event listener to the sign-up button
+  signUpButton.addEventListener('click', function() {
+    container.classList.add('right-panel-active');
+    
+    
+  });
+}
+
+// Add event listener to the sign-in button
+signInButton.addEventListener('click', function() {
+  container.classList.remove('right-panel-active');
+});
+
+// Add event listener to the form submission
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  
+  // You can include further actions here after the form is submitted
+});
+// Call the function to handle the sign-up button click event
+signMeButton();
+
+
+
+
+// Function to handle the sign-up button click event
+
+
   // Function to generate bubbles dynamically on footer
-  function generateBubbles() {
-    var bubble = document.createElement('div');
+  // function generateBubbles() {
+  //   var bubble = document.createElement('div');
 
-    for (let i = 0; i < 128; i++) {
-      var bubble = create('dev');
-      bubble.classList.add('bubble');
+  //   for (let i = 0; i < 128; i++) {
+  //     var bubble = create('dev');
+  //     bubble.classList.add('bubble');
 
-      var size = 2 + Math.random() * 4;
-      var distance = 6 + Math.random() * 4;
-      var position = -5 + Math.random() * 110;
-      var time = 2 + Math.random() * 2;
-      var delay = -1 * (2 + Math.random() * 2);
+  //     var size = 2 + Math.random() * 4;
+  //     var distance = 6 + Math.random() * 4;
+  //     var position = -5 + Math.random() * 110;
+  //     var time = 2 + Math.random() * 2;
+  //     var delay = -1 * (2 + Math.random() * 2);
 
-      bubble.style.setProperty('--size', `${size}rem`);
-      bubble.style.setProperty('--distance', `${distance}rem`);
-      bubble.style.setProperty('--position', `${position}%`);
-      bubble.style.setProperty('--time', `${time}s`);
-      bubble.style.setProperty('--delay', `${delay}s`);
+  //     bubble.style.setProperty('--size', `${size}rem`);
+  //     bubble.style.setProperty('--distance', `${distance}rem`);
+  //     bubble.style.setProperty('--position', `${position}%`);
+  //     bubble.style.setProperty('--time', `${time}s`);
+  //     bubble.style.setProperty('--delay', `${delay}s`);
 
-      bubblesContainer.appendChild(bubble);
-    }
-  };
+  //     bubblesContainer.appendChild(bubble);
+  //   }
+  // };
 
-  // Call the function to generate bubbles
-  generateBubbles();
+  // // Call the function to generate bubbles
+  // generateBubbles();x
