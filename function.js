efd0f08aa42579e7ac5f40e96cc4fbea3d1e0801
https://github.com/kellumschain/contractchain.git <https://github.com/kellumschain/contractchain.git>
2023-09-26 01:41:47 -0400

    updated

---
+++ function.js
@@ -0,0 +1,21 @@
+// Get references to the form and submit button on notification nav bar
+
+function submitForm() {
+  const contentEmail = document.getElementById("email-form");
+  // const submitButton = document.getElementById("my-btn")
+  
+  // Add an event listener to the form submission   
+  contentEmail.addEventListener("submit", function (event) {
+    event.preventDefault();
+    
+
+    // Change the content of the message element and increase display size
+    
+    contentEmail.innerHTML = "Thank you. Your code will automatically apply at checkout.";
+    contentEmail.style.fontSize = "15px";
+    contentEmail.style.color = "white";
+  });
+
+};
+
+//mongodb
